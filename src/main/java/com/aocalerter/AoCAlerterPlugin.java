e1f90a651a4a7c00bc1334a3be20c72778e6bf12
josmoo <joshuamoore22@gmail.com>
2024-11-22 22:33:04 -0600

    corrected offset of bank areas

--- src/main/java/com/aocalerter/AoCAlerterPlugin.java
+++ src/main/java/com/aocalerter/AoCAlerterPlugin.java
@@ -51,120 +51,120 @@ public class AoCAlerterPlugin extends Plugin
 	* if there is an error please let me know
 	* */
 	private static final List<WorldArea> BANK_AREAS = ImmutableList.of(
-			new WorldArea(1246, 3759, 12, 12, 0), // Farming Guild bank
-			new WorldArea(1252, 3570, 12, 12, 0), // Mount Quidamortem bank
-			new WorldArea(1253, 3742, 12, 12, 0), // Farming Guild#Central area|Farming Guild chest
-			new WorldArea(1324, 3825, 12, 12, 0), // Mount Karuulm#Bank|Mount Karuulm chest
-			new WorldArea(1382, 2866, 12, 12, 0), // Mistrock bank
-			new WorldArea(1399, 2927, 12, 12, 0), // Aldarin bank
-			new WorldArea(1438, 3829, 12, 12, 0), // Lovakengj mine bank
-			new WorldArea(1455, 3860, 12, 12, 0), // Sulphur mine#Bank chest|Sulphur mine chest
-			new WorldArea(1456, 9568, 12, 12, 0), // Cam Torum bank
-			new WorldArea(1484, 3644, 12, 12, 0), // War Tent#Bank chests|War Tent chests
-			new WorldArea(1485, 3646, 12, 12, 0), // Shayzien Encampment#Bank|Shayzien Encampment bank chest
-			new WorldArea(1487, 3593, 12, 12, 0), // Shayzien bank
-			new WorldArea(1489, 3864, 12, 12, 0), // Blast mine#Bank chests|Blast mine chests
-			new WorldArea(1513, 3421, 12, 12, 0), // Land's End#Bank chest|Land's End chest
-			new WorldArea(1520, 3229, 12, 12, 0), // Quetzacalli Gorge bank
-			new WorldArea(1526, 3293, 12, 12, 0), // The Hueycoatl|The Darkfrost bank
-			new WorldArea(1526, 3739, 12, 12, 0), // Lovakengj bank
-			new WorldArea(1542, 3040, 12, 12, 0), // Hunter Guild#Bank|Hunter Guild bank
-			new WorldArea(1549, 9875, 12, 12, 0), // Woodcutting Guild#Bank chest 2|Ent dungeon chest
-			new WorldArea(1589, 3475, 12, 12, 0), // Woodcutting Guild#Bank chest|Woodcutting Guild chest
-			new WorldArea(1612, 3682, 12, 12, 2), // Kourend Castle bank
-			new WorldArea(1630, 3745, 12, 12, 0), // Arceuus bank
-			new WorldArea(1638, 3942, 12, 12, 0), // Wintertodt#Bank chest|Wintertodt chest
-			new WorldArea(1648, 3118, 12, 12, 0), // Fortis west bank
-			new WorldArea(1676, 3616, 12, 12, 0), // Hosidius Kitchen#Layout|Hosidius Kitchen chest
-			new WorldArea(1717, 3464, 12, 12, 0), // Charcoal furnace#Bank chest|Charcoal camp chest
-			new WorldArea(1748, 5476, 12, 12, 0), // Clan Hall#Features|Clan Hall bank
-			new WorldArea(1748, 3599, 12, 12, 0), // Hosidius bank
-			new WorldArea(1780, 3096, 12, 12, 0), // Fortis east bank
-			new WorldArea(1803, 3788, 12, 12, 0), // Port Piscarilius bank
-			new WorldArea(1805, 9501, 12, 12, 0), // Fortis Colosseum#Lobby area|Fortis Colosseum chest
-			new WorldArea(1809, 3567, 12, 12, 0), // Vinery#Bank chest|Vinery chest
-			new WorldArea(2101, 3919, 12, 12, 0), // Lunar Isle bank
-			new WorldArea(2330, 3690, 12, 12, 0), // Piscatoris bank
-			new WorldArea(2337, 3807, 12, 12, 0), // Neitiznot bank
-			new WorldArea(2352, 3162, 12, 12, 0), // Lletya bank
-			new WorldArea(2383, 4459, 12, 12, 0), // Zanaris bank
-			new WorldArea(2400, 5983, 12, 12, 0), // Hallowed Sepulchre
-			new WorldArea(2417, 3801, 12, 12, 0), // Jatizso bank
-			new WorldArea(2444, 3485, 12, 12, 1), // Gnome Stronghold north bank
-			new WorldArea(2445, 3082, 12, 12, 0), // Castle Wars#Main lobby|Castle Wars lobby chest
-			new WorldArea(2446, 5179, 12, 12, 0), // Mor Ul Rek north bank|Tzhaar City bank
-			new WorldArea(2446, 3424, 12, 12, 0), // Gnome Stronghold south bank
-			new WorldArea(2463, 2848, 12, 12, 1), // Myths' Guild#First floor|Myths' Guild chest
-			new WorldArea(2537, 3574, 12, 12, 0), // Barbarian Outpost#Bank chest|Barbarian Outpost chest
-			new WorldArea(2543, 5144, 12, 12, 0), // Mor Ul Rek east bank
-			new WorldArea(2568, 2863, 12, 12, 0), // Corsair Cove bank
-			new WorldArea(2588, 3416, 12, 12, 0), // Fishing Guild bank
-			new WorldArea(2613, 3093, 12, 12, 0), // Yanille bank
-			new WorldArea(2617, 3333, 12, 12, 0), // Ardougne north bank
-			new WorldArea(2620, 3895, 12, 12, 0), // Etceteria bank
-			new WorldArea(2653, 3284, 12, 12, 0), // Ardougne south bank
-			new WorldArea(2662, 3161, 12, 12, 0), // Port Khazard#Bank chest|Port Khazard chest
-			new WorldArea(2667, 2653, 12, 12, 0), // Void Knights' Outpost bank
-			new WorldArea(2702, 5350, 12, 12, 0), // Dorgesh-Kaan bank
-			new WorldArea(2725, 3492, 12, 12, 0), // Seers' Village bank|Seersâ€™ Village bank
-			new WorldArea(2733, 3379, 12, 12, 2), // Legends' Guild bank
-			new WorldArea(2757, 3478, 12, 12, 0), // PvP world#Bank chests|Camelot PvP chest
-			new WorldArea(2781, 2784, 12, 12, 0), // Ape Atoll bank
-			new WorldArea(2810, 3442, 12, 12, 0), // Catherby bank
-			new WorldArea(2838, 10210, 12, 12, 0), // Keldagrim bank
-			new WorldArea(2843, 3543, 12, 12, 0), // Warriors' Guild bank|Warriorsâ€™ Guild bank
-			new WorldArea(2852, 2955, 12, 12, 0), // Shilo Village bank
-			new WorldArea(2904, 5205, 12, 12, 0), // Ashuelot Reis|Ancient Prison bank
-			new WorldArea(2931, 10195, 12, 12, 0), // Blast Furnace#Bank chest|Blast Furnace bank
-			new WorldArea(2936, 3280, 12, 12, 0), // Crafting Guild#Bank chest|Crafting Guild chest
-			new WorldArea(2946, 3367, 12, 12, 0), // Falador west bank
-			new WorldArea(2970, 3342, 12, 12, 0), // PvP world#Bank chests|Falador PvP chest
-			new WorldArea(2978, 5799, 12, 12, 0), // Ruins of Camdozaal#Bank|Camdozaal chest
-			new WorldArea(3012, 9718, 12, 12, 0), // Mining Guild#Bank chest|Mining Guild chest
-			new WorldArea(3015, 3357, 12, 12, 0), // Falador east bank
-			new WorldArea(3016, 5625, 12, 12, 0), // Ourania Altar|Ourania bank
-			new WorldArea(3042, 4972, 12, 12, 0), // Emerald Benedict|Emerald Benedict's bank
-			new WorldArea(3090, 3956, 12, 12, 0), // Mage Arena bank
-			new WorldArea(3093, 3244, 12, 12, 0), // Draynor bank
-			new WorldArea(3094, 3469, 12, 12, 0), // PvP world#Bank chests|Edgeville PvP chest
-			new WorldArea(3095, 3494, 12, 12, 0), // Edgeville bank
-			new WorldArea(3096, 3027, 12, 12, 0), // The Node
-			new WorldArea(3119, 9698, 12, 12, 0), // Motherlode Mine#Bank chest|Motherload Mine chest
-			new WorldArea(3122, 3124, 12, 12, 0), // Tutorial Island bank
-			new WorldArea(3130, 3632, 12, 12, 0), // Ferox Enclave#Bank|Ferox Enclave bank
-			new WorldArea(3148, 3448, 12, 12, 0), // Cooks' Guild#Bank|Cook's Guild bank
-			new WorldArea(3156, 2837, 12, 12, 0), // Ruins of Unkah#Bank|Unkah chest
-			new WorldArea(3165, 3490, 12, 12, 0), // Grand Exchange
-			new WorldArea(3185, 3441, 12, 12, 0), // Varrock west bank
-			new WorldArea(3195, 4570, 12, 12, 0), // Odovacar|Tarn's Lair bank
-			new WorldArea(3209, 3219, 12, 12, 2), // Lumbridge Castle bank
-			new WorldArea(3219, 9623, 12, 12, 0), // Culinaromancer's Chest|Culinaromancerâ€™s Chest
-			new WorldArea(3221, 3219, 12, 12, 0), // PvP world#Bank chests|Lumbridge PvP chest
-			new WorldArea(3254, 3421, 12, 12, 0), // Varrock east bank
-			new WorldArea(3257, 6108, 12, 12, 0), // Prifddinas north bank
-			new WorldArea(3269, 3167, 12, 12, 0), // Al Kharid bank
-			new WorldArea(3278, 5201, 12, 12, 0), // Chambers of Xeric|Chambers of Xeric lobby chest
-			new WorldArea(3297, 6060, 12, 12, 0), // Prifddinas south bank
-			new WorldArea(3303, 5199, 12, 12, 0), // Chambers of Xeric|Chambers of Xeric lobby chest2
-			new WorldArea(3308, 3119, 12, 12, 0), // Shantay Pass#Bank|Shantay Pass bank
-			new WorldArea(3312, 2799, 12, 12, 0), // Sophanem bank
-			new WorldArea(3333, 5195, 12, 12, 0), // Chambers of Xeric|Chambers of Xeric lobby chest3
-			new WorldArea(3345, 2725, 12, 12, 0), // Tombs of Amascut#Lobby|Tombs of Amascut bank
-			new WorldArea(3367, 3318, 12, 12, 1), // Mage Training Arena|Mage Training Arena chest
-			new WorldArea(3384, 3270, 12, 12, 0), // Emir's Arena#Bank|Emir's Arena bank
-			new WorldArea(3426, 4064, 12, 12, 0), // Daimon's Crater#Bank chests|Daimon's Crater chests
-			new WorldArea(3427, 2891, 12, 12, 0), // Nardah bank
-			new WorldArea(3496, 3211, 12, 12, 0), // Burgh de Rott bank
-			new WorldArea(3512, 3478, 12, 12, 0), // Canifis bank
-			new WorldArea(3602, 3366, 12, 12, 0), // Darkmeyer bank
-			new WorldArea(3619, 9473, 12, 12, 0), // Guardians of the Rift#Bank chest|Guardians of the Rift chest
-			new WorldArea(3648, 3204, 12, 12, 0), // Ver Sinhaza bank
-			new WorldArea(3681, 2982, 12, 12, 0), // Mos Le'Harmless bank|Mos Leâ€™Harmless bank
-			new WorldArea(3692, 3468, 12, 12, 0), // Port Phasmatys bank
-			new WorldArea(3739, 3802, 12, 12, 0), // Fossil Island bank
-			new WorldArea(3772, 3898, 12, 12, 0), // Bank Chest-wreck
-			new WorldArea(3810, 3020, 12, 12, 0), // Trouble Brewing#Bank chest|Trouble Brewing chest
-			new WorldArea(3819, 3809, 12, 12, 0) // Volcanic Mine#Bank chest|Volcanic Mine chest
+			new WorldArea(1240, 3753, 12, 12, 0), // Farming Guild bank
+			new WorldArea(1246, 3564, 12, 12, 0), // Mount Quidamortem bank
+			new WorldArea(1247, 3736, 12, 12, 0), // Farming Guild#Central area|Farming Guild chest
+			new WorldArea(1318, 3819, 12, 12, 0), // Mount Karuulm#Bank|Mount Karuulm chest
+			new WorldArea(1376, 2860, 12, 12, 0), // Mistrock bank
+			new WorldArea(1393, 2921, 12, 12, 0), // Aldarin bank
+			new WorldArea(1432, 3823, 12, 12, 0), // Lovakengj mine bank
+			new WorldArea(1449, 3854, 12, 12, 0), // Sulphur mine#Bank chest|Sulphur mine chest
+			new WorldArea(1450, 9562, 12, 12, 0), // Cam Torum bank
+			new WorldArea(1478, 3638, 12, 12, 0), // War Tent#Bank chests|War Tent chests
+			new WorldArea(1479, 3640, 12, 12, 0), // Shayzien Encampment#Bank|Shayzien Encampment bank chest
+			new WorldArea(1481, 3587, 12, 12, 0), // Shayzien bank
+			new WorldArea(1483, 3858, 12, 12, 0), // Blast mine#Bank chests|Blast mine chests
+			new WorldArea(1507, 3415, 12, 12, 0), // Land's End#Bank chest|Land's End chest
+			new WorldArea(1514, 3223, 12, 12, 0), // Quetzacalli Gorge bank
+			new WorldArea(1520, 3287, 12, 12, 0), // The Hueycoatl|The Darkfrost bank
+			new WorldArea(1520, 3733, 12, 12, 0), // Lovakengj bank
+			new WorldArea(1536, 3034, 12, 12, 0), // Hunter Guild#Bank|Hunter Guild bank
+			new WorldArea(1543, 9869, 12, 12, 0), // Woodcutting Guild#Bank chest 2|Ent dungeon chest
+			new WorldArea(1583, 3469, 12, 12, 0), // Woodcutting Guild#Bank chest|Woodcutting Guild chest
+			new WorldArea(1606, 3676, 12, 12, 2), // Kourend Castle bank
+			new WorldArea(1624, 3739, 12, 12, 0), // Arceuus bank
+			new WorldArea(1632, 3936, 12, 12, 0), // Wintertodt#Bank chest|Wintertodt chest
+			new WorldArea(1642, 3112, 12, 12, 0), // Fortis west bank
+			new WorldArea(1670, 3610, 12, 12, 0), // Hosidius Kitchen#Layout|Hosidius Kitchen chest
+			new WorldArea(1711, 3458, 12, 12, 0), // Charcoal furnace#Bank chest|Charcoal camp chest
+			new WorldArea(1742, 5470, 12, 12, 0), // Clan Hall#Features|Clan Hall bank
+			new WorldArea(1742, 3593, 12, 12, 0), // Hosidius bank
+			new WorldArea(1774, 3090, 12, 12, 0), // Fortis east bank
+			new WorldArea(1797, 3782, 12, 12, 0), // Port Piscarilius bank
+			new WorldArea(1799, 9495, 12, 12, 0), // Fortis Colosseum#Lobby area|Fortis Colosseum chest
+			new WorldArea(1803, 3561, 12, 12, 0), // Vinery#Bank chest|Vinery chest
+			new WorldArea(2095, 3913, 12, 12, 0), // Lunar Isle bank
+			new WorldArea(2324, 3684, 12, 12, 0), // Piscatoris bank
+			new WorldArea(2331, 3801, 12, 12, 0), // Neitiznot bank
+			new WorldArea(2346, 3156, 12, 12, 0), // Lletya bank
+			new WorldArea(2377, 4453, 12, 12, 0), // Zanaris bank
+			new WorldArea(2394, 5977, 12, 12, 0), // Hallowed Sepulchre
+			new WorldArea(2411, 3795, 12, 12, 0), // Jatizso bank
+			new WorldArea(2438, 3479, 12, 12, 1), // Gnome Stronghold north bank
+			new WorldArea(2439, 3076, 12, 12, 0), // Castle Wars#Main lobby|Castle Wars lobby chest
+			new WorldArea(2440, 5173, 12, 12, 0), // Mor Ul Rek north bank|Tzhaar City bank
+			new WorldArea(2440, 3418, 12, 12, 0), // Gnome Stronghold south bank
+			new WorldArea(2457, 2842, 12, 12, 1), // Myths' Guild#First floor|Myths' Guild chest
+			new WorldArea(2531, 3568, 12, 12, 0), // Barbarian Outpost#Bank chest|Barbarian Outpost chest
+			new WorldArea(2537, 5138, 12, 12, 0), // Mor Ul Rek east bank
+			new WorldArea(2562, 2857, 12, 12, 0), // Corsair Cove bank
+			new WorldArea(2582, 3410, 12, 12, 0), // Fishing Guild bank
+			new WorldArea(2607, 3087, 12, 12, 0), // Yanille bank
+			new WorldArea(2611, 3327, 12, 12, 0), // Ardougne north bank
+			new WorldArea(2614, 3889, 12, 12, 0), // Etceteria bank
+			new WorldArea(2647, 3278, 12, 12, 0), // Ardougne south bank
+			new WorldArea(2656, 3155, 12, 12, 0), // Port Khazard#Bank chest|Port Khazard chest
+			new WorldArea(2661, 2647, 12, 12, 0), // Void Knights' Outpost bank
+			new WorldArea(2696, 5344, 12, 12, 0), // Dorgesh-Kaan bank
+			new WorldArea(2719, 3486, 12, 12, 0), // Seers' Village bank|Seersâ€™ Village bank
+			new WorldArea(2727, 3373, 12, 12, 2), // Legends' Guild bank
+			new WorldArea(2751, 3472, 12, 12, 0), // PvP world#Bank chests|Camelot PvP chest
+			new WorldArea(2775, 2778, 12, 12, 0), // Ape Atoll bank
+			new WorldArea(2804, 3436, 12, 12, 0), // Catherby bank
+			new WorldArea(2832, 10204, 12, 12, 0), // Keldagrim bank
+			new WorldArea(2837, 3537, 12, 12, 0), // Warriors' Guild bank|Warriorsâ€™ Guild bank
+			new WorldArea(2846, 2949, 12, 12, 0), // Shilo Village bank
+			new WorldArea(2898, 5199, 12, 12, 0), // Ashuelot Reis|Ancient Prison bank
+			new WorldArea(2925, 10189, 12, 12, 0), // Blast Furnace#Bank chest|Blast Furnace bank
+			new WorldArea(2930, 3274, 12, 12, 0), // Crafting Guild#Bank chest|Crafting Guild chest
+			new WorldArea(2940, 3361, 12, 12, 0), // Falador west bank
+			new WorldArea(2964, 3336, 12, 12, 0), // PvP world#Bank chests|Falador PvP chest
+			new WorldArea(2972, 5793, 12, 12, 0), // Ruins of Camdozaal#Bank|Camdozaal chest
+			new WorldArea(3006, 9712, 12, 12, 0), // Mining Guild#Bank chest|Mining Guild chest
+			new WorldArea(3009, 3351, 12, 12, 0), // Falador east bank
+			new WorldArea(3010, 5619, 12, 12, 0), // Ourania Altar|Ourania bank
+			new WorldArea(3036, 4966, 12, 12, 0), // Emerald Benedict|Emerald Benedict's bank
+			new WorldArea(3084, 3950, 12, 12, 0), // Mage Arena bank
+			new WorldArea(3087, 3238, 12, 12, 0), // Draynor bank
+			new WorldArea(3088, 3463, 12, 12, 0), // PvP world#Bank chests|Edgeville PvP chest
+			new WorldArea(3089, 3488, 12, 12, 0), // Edgeville bank
+			new WorldArea(3090, 3021, 12, 12, 0), // The Node
+			new WorldArea(3113, 9692, 12, 12, 0), // Motherlode Mine#Bank chest|Motherload Mine chest
+			new WorldArea(3116, 3118, 12, 12, 0), // Tutorial Island bank
+			new WorldArea(3124, 3626, 12, 12, 0), // Ferox Enclave#Bank|Ferox Enclave bank
+			new WorldArea(3142, 3442, 12, 12, 0), // Cooks' Guild#Bank|Cook's Guild bank
+			new WorldArea(3150, 2831, 12, 12, 0), // Ruins of Unkah#Bank|Unkah chest
+			new WorldArea(3159, 3484, 12, 12, 0), // Grand Exchange
+			new WorldArea(3179, 3435, 12, 12, 0), // Varrock west bank
+			new WorldArea(3189, 4564, 12, 12, 0), // Odovacar|Tarn's Lair bank
+			new WorldArea(3203, 3213, 12, 12, 2), // Lumbridge Castle bank
+			new WorldArea(3213, 9617, 12, 12, 0), // Culinaromancer's Chest|Culinaromancerâ€™s Chest
+			new WorldArea(3215, 3213, 12, 12, 0), // PvP world#Bank chests|Lumbridge PvP chest
+			new WorldArea(3248, 3415, 12, 12, 0), // Varrock east bank
+			new WorldArea(3251, 6102, 12, 12, 0), // Prifddinas north bank
+			new WorldArea(3263, 3161, 12, 12, 0), // Al Kharid bank
+			new WorldArea(3272, 5195, 12, 12, 0), // Chambers of Xeric|Chambers of Xeric lobby chest
+			new WorldArea(3291, 6054, 12, 12, 0), // Prifddinas south bank
+			new WorldArea(3297, 5193, 12, 12, 0), // Chambers of Xeric|Chambers of Xeric lobby chest2
+			new WorldArea(3302, 3113, 12, 12, 0), // Shantay Pass#Bank|Shantay Pass bank
+			new WorldArea(3306, 2793, 12, 12, 0), // Sophanem bank
+			new WorldArea(3327, 5189, 12, 12, 0), // Chambers of Xeric|Chambers of Xeric lobby chest3
+			new WorldArea(3339, 2719, 12, 12, 0), // Tombs of Amascut#Lobby|Tombs of Amascut bank
+			new WorldArea(3361, 3312, 12, 12, 1), // Mage Training Arena|Mage Training Arena chest
+			new WorldArea(3378, 3264, 12, 12, 0), // Emir's Arena#Bank|Emir's Arena bank
+			new WorldArea(3420, 4058, 12, 12, 0), // Daimon's Crater#Bank chests|Daimon's Crater chests
+			new WorldArea(3421, 2885, 12, 12, 0), // Nardah bank
+			new WorldArea(3490, 3205, 12, 12, 0), // Burgh de Rott bank
+			new WorldArea(3506, 3472, 12, 12, 0), // Canifis bank
+			new WorldArea(3596, 3360, 12, 12, 0), // Darkmeyer bank
+			new WorldArea(3613, 9467, 12, 12, 0), // Guardians of the Rift#Bank chest|Guardians of the Rift chest
+			new WorldArea(3642, 3198, 12, 12, 0), // Ver Sinhaza bank
+			new WorldArea(3675, 2976, 12, 12, 0), // Mos Le'Harmless bank|Mos Leâ€™Harmless bank
+			new WorldArea(3686, 3462, 12, 12, 0), // Port Phasmatys bank
+			new WorldArea(3733, 3796, 12, 12, 0), // Fossil Island bank
+			new WorldArea(3766, 3892, 12, 12, 0), // Bank Chest-wreck
+			new WorldArea(3804, 3014, 12, 12, 0), // Trouble Brewing#Bank chest|Trouble Brewing chest
+			new WorldArea(3813, 3803, 12, 12, 0)  // Volcanic Mine#Bank chest|Volcanic Mine chest
 	);
 
 
